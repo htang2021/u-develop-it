cfdeb9765ef342bc39cfd3b1630b5a19363f6f60
Hung Tang <hungtang@hotmail.com>
2021-04-18 22:01:46 -0700

    added DB connection from express server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,7 +8,15 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 
 // Default response for any other request (Not Found)
